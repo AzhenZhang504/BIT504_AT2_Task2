a398e03eae88ff57cfae738d7ba2ee6e4fafab7e
AzhenZhang504 <369159004@qq.com>
2023-02-10 22:17:13 +1300

    //TODO: create the new GameMain panel and add it to the frame //TODO: set the default close operation of the frame to exit_on_close

--- BIT504_Assign2_PartAStudent/GameMain.java
+++ BIT504_Assign2_PartAStudent/GameMain.java
@@ -71,11 +71,12 @@ public class GameMain extends JPanel implements MouseListener{
 				JFrame frame = new JFrame(TITLE);
 				
 				//TODO: create the new GameMain panel and add it to the frame
-						
+				GameMain gameMain = new GameMain();
+				frame.add(gameMain);
 				
 				
 				//TODO: set the default close operation of the frame to exit_on_close
-		            
+		        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				
 				frame.pack();             
 				frame.setLocationRelativeTo(null);
