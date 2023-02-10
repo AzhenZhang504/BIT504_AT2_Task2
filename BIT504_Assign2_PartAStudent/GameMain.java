7c86989d2e037600316bcb331ea50fee81d5dd26
AzhenZhang504 <369159004@qq.com>
2023-02-10 22:56:34 +1300

    //TODO: Create a new instance of the game "Board"class //TODO: call the method to initialise the game board

--- BIT504_Assign2_PartAStudent/GameMain.java
+++ BIT504_Assign2_PartAStudent/GameMain.java
@@ -57,10 +57,10 @@ public class GameMain extends JPanel implements MouseListener{
 		
 		
 		// TODO: Create a new instance of the game "Board"class. HINT check the variables above for the correct name
-
+        Board board = new Board();
 		
 		//TODO: call the method to initialise the game board
-
+        this.board = board;
 	}
 	
 	public static void main(String[] args) {
